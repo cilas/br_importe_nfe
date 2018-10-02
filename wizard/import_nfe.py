40c8ce8b123c45153e4dffad50c637512278be6d
maiconkkl <ma_bonitao@hotmail.com>
2018-10-02 09:45:02 -0300

    Correção do erro de local variable
    Erro:
    Odoo Server Error
    
    Traceback (most recent call last):
      File "/home/cilas/shared/odoo/odoo/http.py", line 650, in _handle_exception
        return super(JsonRequest, self)._handle_exception(exception)
      File "/home/cilas/shared/odoo/odoo/http.py", line 310, in _handle_exception
        raise pycompat.reraise(type(exception), exception, sys.exc_info()[2])
      File "/home/cilas/shared/odoo/odoo/tools/pycompat.py", line 87, in reraise
        raise value
      File "/home/cilas/shared/odoo/odoo/http.py", line 692, in dispatch
        result = self._call_function(**self.params)
      File "/home/cilas/shared/odoo/odoo/http.py", line 342, in _call_function
        return checked_call(self.db, *args, **kwargs)
      File "/home/cilas/shared/odoo/odoo/service/model.py", line 97, in wrapper
        return f(dbname, *args, **kwargs)
      File "/home/cilas/shared/odoo/odoo/http.py", line 335, in checked_call
        result = self.endpoint(*a, **kw)
      File "/home/cilas/shared/odoo/odoo/http.py", line 936, in call
        return self.method(*args, **kw)
      File "/home/cilas/shared/odoo/odoo/http.py", line 515, in response_wrap
        response = f(*args, **kw)
      File "/home/cilas/shared/odoo/addons/web/controllers/main.py", line 938, in call_button
        action = self._call_kw(model, method, args, {})
      File "/home/cilas/shared/odoo/addons/web/controllers/main.py", line 926, in _call_kw
        return call_kw(request.env[model], method, args, kwargs)
      File "/home/cilas/shared/odoo/odoo/api.py", line 689, in call_kw
        return call_kw_multi(method, model, args, kwargs)
      File "/home/cilas/shared/odoo/odoo/api.py", line 680, in call_kw_multi
        result = method(recs, *args, **kwargs)
      File "/home/cilas/shared/odoo-brasil/br_importe_nfe/wizard/import_nfe.py", line 92, in action_import_nfe_purchase
        dest=nfe.NFe.infNFe.dest)
      File "/home/cilas/shared/odoo-brasil/br_importe_nfe/wizard/import_nfe.py", line 115, in purchase_order_line
        preco_unitario = prod.prod.vProd / quantidade
    UnboundLocalError: local variable 'quantidade' referenced before assignment

--- wizard/import_nfe.py
+++ wizard/import_nfe.py
@@ -84,6 +84,10 @@ class WizardImportNfe(models.TransientModel):
             total_frete=nfe.NFe.infNFe.total.ICMSTot.vFrete,
             picking_count=1,
         )
+
+        if hasattr(nfe.NFe.infNFe, 'infAdic') and hasattr(nfe.NFe.infNFe.infAdic, 'infCpl'):
+            nota_dict['notes'] = nfe.NFe.infNFe.infAdic.infCpl.text
+
         nota = self.env['purchase.order'].create(nota_dict)
 
         # Cria os itens na nota
@@ -94,7 +98,184 @@ class WizardImportNfe(models.TransientModel):
         # Vamos alterar inserir os itens criados na nota
         nota_dict = {'order_line': items}
         nota.write(nota_dict)
-        nota._compute_tax_id()
+
+    @staticmethod
+    def arruma_cpf_cnpj(partner_doc):
+        if len(partner_doc) > 11:
+            if len(partner_doc) < 14:
+                partner_doc = partner_doc.zfill(14)
+            partner_doc = "%s.%s.%s/%s-%s" % (partner_doc[0:2],
+                                              partner_doc[2:5],
+                                              partner_doc[5:8],
+                                              partner_doc[8:12],
+                                              partner_doc[12:14])
+        else:
+            if len(partner_doc) < 11:
+                partner_doc = partner_doc.zfill(11)
+            partner_doc = "%s.%s.%s-%s" % (partner_doc[0:3],
+                                           partner_doc[3:6],
+                                           partner_doc[6:9],
+                                           partner_doc[9:11])
+        return partner_doc
+
+    def cadastro_de_produto(self, produto_wizard, produto_xml, preco_unitario):
+        vals = {
+            'name': produto_xml.xProd.text,
+            'default_code': produto_xml.cProd.text,
+            'type': 'product',
+            'list_price': preco_unitario,
+            'purchase_method': 'receive'
+        }
+
+        if produto_wizard.uom_int:
+            vals['product_uom_xml_id'] = produto_wizard.uom_int.id
+        else:
+            raise UserError('A unidade medida interna do produto: ' + produto_wizard.name + ' é obrigatoria')
+
+        if hasattr(produto_xml, 'CEST'):
+            vals['cest'] = produto_xml.CEST
+
+        if hasattr(produto_xml, 'rastro'):
+            vals['tracking'] = 'lot'
+        else:
+            vals['tracking'] = 'none'
+
+        pf_ids = self.env['product.fiscal.classification'].search([('code', '=', produto_xml.NCM)])
+        vals['fiscal_classification_id'] = pf_ids.id
+        product_id = self.env['product.product'].create(vals)
+        return product_id
+
+    def carrega_produtos(self, item, nfe):
+        product_find = False
+        product_create = {'name': item.prod.xProd, 'uom_int': 1}
+
+        uom_id = self.env['product.uom'].search([
+            ('name', '=', str(item.prod.uCom))], limit=1).id
+
+        if uom_id:
+            product_create['uom_ext'] = uom_id
+
+        if item.prod.cEAN != 'SEM GTIN' and item.prod.cEAN != '':
+            product_find = self.env['product.product'].search([
+                ('barcode', '=', item.prod.cEAN)], limit=1)
+
+            if product_find:
+                product_create['product_id'] = product_find.id
+
+        if not product_find:
+            if hasattr(nfe.NFe.infNFe.emit, 'CNPJ'):
+                partner_doc = nfe.NFe.infNFe.emit.CNPJ.text
+            else:
+                partner_doc = nfe.NFe.infNFe.emit.CPF.text
+
+            partner_id = self.env['res.partner'].search([
+                ('cnpj_cpf', '=', self.arruma_cpf_cnpj(partner_doc))
+            ]).id
+            product_code = self.env['product.supplierinfo'].search([
+                ('product_code', '=', item.prod.cProd.text),
+                ('name', '=', partner_id)
+            ], limit=1)
+            product_find = self.env['product.product'].browse(product_code.product_tmpl_id.id)
+
+            if product_find:
+                product_create['product_id'] = product_find.id
+
+        return self.env['wizard.produtos'].create(product_create)
+
+    def checa_produtos(self):
+        if not self.nfe_xml:
+            raise UserError('Por favor, insira um arquivo de NFe.')
+        nfe_string = base64.b64decode(self.nfe_xml)
+        nfe = objectify.fromstring(nfe_string)
+
+        if not hasattr(nfe, 'NFe'):
+            raise UserError('Lamento, mas isso não é uma nota fiscal valida.')
+
+        company = self.get_partner(partner_find=nfe.NFe.infNFe.dest)
+        if not company:
+            raise UserError("Essa nota não é sua ou seu emitente não esta configurado corretamente.")
+
+        items = []
+        for det in nfe.NFe.infNFe.det:
+            item = self.carrega_produtos(det, nfe)
+            if item:
+                items.append(item.id)
+        self.wizard_produtos = self.env['wizard.produtos'].browse(items)
+        self.confirma = True
+
+        return {
+            'context': self.env.context,
+            'view_type': 'form',
+            'view_mode': 'form',
+            'res_model': 'wizard.import.nfe',
+            'res_id': self.id,
+            'view_id': False,
+            'type': 'ir.actions.act_window',
+            'target': 'new',
+        }
+
+    def criar_fatura(self, purchase_order):
+        invoice_dict = {
+            'origin': purchase_order.name,
+            'type': 'in_invoice',
+            'comment': u'FATURA GERADA AUTOMATICAMENTE ATRAVES DE IMPORATACAO DE DOCUMENTO ELETRONICO\n'
+                       u'CHAVE DA NFE DE ORIGEM: ' + purchase_order.chave_nfe,
+            'fiscal_comment': purchase_order.notes,
+        }
+
+    def get_partner(self, partner_find, create=False, custumer=False, supplier=False):
+        partner_doc = partner_find.CNPJ if hasattr(partner_find, 'CNPJ') else partner_find.CPF
+        partner_doc = str(partner_doc)
+        partner_doc = self.arruma_cpf_cnpj(partner_doc)
+        partner = self.env['res.partner'].search([
+            ('cnpj_cpf', '=', partner_doc)])
+
+        # Empresa não encontrado, devemos criar ?
+        if not partner and create:
+            # Ok, então vamos criar a empresa
+            city = self.env['res.state.city'].search([('name', '=ilike', partner_find.enderEmit.xMun)])
+            partner = {
+                'name': partner_find.xNome,
+                'is_company': True if hasattr(partner_find, 'CNPJ') else False,
+                'company_type': 'company' if hasattr(partner_find, 'CNPJ') else 'person',
+                'cnpj_cpf': partner_doc,
+                'supplier': True,
+            }
+
+            if custumer:
+                partner['custumer'] = True
+
+            if supplier:
+                partner['supplier'] = True
+
+            if hasattr(partner_find, 'IE') and hasattr(partner_find, 'CNPJ'):
+                partner['inscr_est'] = partner_find.IE.text
+                partner['indicador_ie_dest'] = '1'
+            elif not hasattr(partner_find, 'IE') and hasattr(partner_find, 'CNPJ'):
+                partner['indicador_ie_dest'] = '9'
+
+            if hasattr(partner_find, 'xFant'):
+                partner['legal_name'] = partner_find.xFant
+            else:
+                partner['legal_name'] = partner_find.xNome
+
+            if hasattr(partner_find, 'fone'):
+                partner['phone'] = partner_find.enderEmit.fone
+
+            if hasattr(partner_find, 'xCpl'):
+                partner['street2'] = partner_find.enderEmit.xCpl
+
+            partner['zip'] = partner_find.enderEmit.CEP
+            partner['default_supplier'] = 'supplier'
+            partner['street'] = partner_find.enderEmit.xLgr
+            partner['number'] = partner_find.enderEmit.nro
+            partner['district'] = partner_find.enderEmit.xBairro
+            partner['city_id'] = city.id
+            partner['state_id'] = city.state_id.id
+            partner['country_id'] = self.env.ref('base.br').id
+            partner = self.env['res.partner'].create(partner)
+
+        return partner
 
     def purchase_order_line(self, order, itens, dest):
         # Variaveis uteis
@@ -105,17 +286,18 @@ class WizardImportNfe(models.TransientModel):
         for produto in self.wizard_produtos:
             for prod in itens:
                 if produto.name == prod.prod.xProd:
+                    # Definindo variaveis de acordo com produto, pode muda com fator
+                    quantidade = prod.prod.qCom
+                    preco_unitario = prod.prod.vUnCom
+
                     # Verificando se o produto tem fator de conversão
                     if hasattr(produto.fator, 'id'):
                         # Aplicando fator de conversão
                         if produto.fator.tipo == '0':
-                            quantidade = prod.prod.qCom * produto.fator.valor
+                            quantidade = quantidade * produto.fator.valor
                         elif produto.fator.tipo == '1':
-                            quantidade = prod.prod.qCom / produto.fator.valor
+                            quantidade = quantidade / produto.fator.valor
                         preco_unitario = prod.prod.vProd / quantidade
-                    else:
-                        quantidade = prod.prod.qCom
-                        preco_unitario = prod.prod.vUnCom
 
                     # Verificando se a unidade medida externa foi informada
                     if not produto.uom_ext:
@@ -188,7 +370,7 @@ class WizardImportNfe(models.TransientModel):
                             icms.vICMS > 0 and \
                             dest.indIEDest == 1 and \
                             cfop.name.count("Industrialização") > 0:
-                                purchase_order_line_dict['incluir_ipi_base'] = False
+                        purchase_order_line_dict['incluir_ipi_base'] = False
 
                     # Verificando se esse produto não vai ter ipi na base por Comercialização conforme
                     # Constituição (art. 155, §2°, XI) e Lei Kandir (LC 87/96, art. 13, §2°)
@@ -198,7 +380,7 @@ class WizardImportNfe(models.TransientModel):
                             icms.vICMS > 0 and \
                             dest.indIEDest == 1 and \
                             cfop.name.count("Comercialização") > 0:
-                                purchase_order_line_dict['incluir_ipi_base'] = False
+                        purchase_order_line_dict['incluir_ipi_base'] = False
 
                     # Produto vai ter a ipi na base conforme
                     # Constituição (art. 155, §2°, XI) e Lei Kandir (LC 87/96, art. 13, §2°)
@@ -231,52 +413,6 @@ class WizardImportNfe(models.TransientModel):
 
         return items
 
-    @staticmethod
-    def arruma_cpf_cnpj(partner_doc):
-        if len(partner_doc) > 11:
-            if len(partner_doc) < 14:
-                partner_doc = partner_doc.zfill(14)
-            partner_doc = "%s.%s.%s/%s-%s" % (partner_doc[0:2],
-                                              partner_doc[2:5],
-                                              partner_doc[5:8],
-                                              partner_doc[8:12],
-                                              partner_doc[12:14])
-        else:
-            if len(partner_doc) < 11:
-                partner_doc = partner_doc.zfill(11)
-            partner_doc = "%s.%s.%s-%s" % (partner_doc[0:3],
-                                           partner_doc[3:6],
-                                           partner_doc[6:9],
-                                           partner_doc[9:11])
-        return partner_doc
-
-    def cadastro_de_produto(self, produto_wizard, produto_xml, preco_unitario):
-        vals = {
-            'name': produto_xml.xProd.text,
-            'default_code': produto_xml.cProd.text,
-            'type': 'product',
-            'list_price': preco_unitario,
-            'purchase_method': 'receive'
-        }
-
-        if produto_wizard.uom_int:
-            vals['product_uom_xml_id'] = produto_wizard.uom_int.id
-        else:
-            raise UserError('A unidade medida interna do produto: ' + produto_wizard.name + ' é obrigatoria')
-
-        if hasattr(produto_xml, 'CEST'):
-            vals['cest'] = produto_xml.CEST
-
-        if hasattr(produto_xml, 'rastro'):
-            vals['tracking'] = 'lot'
-        else:
-            vals['tracking'] = 'none'
-
-        pf_ids = self.env['product.fiscal.classification'].search([('code', '=', produto_xml.NCM)])
-        vals['fiscal_classification_id'] = pf_ids.id
-        product_id = self.env['product.product'].create(vals)
-        return product_id
-
     def relacionamento_produto_fornecedor(self, partner, produto, item):
         # cadastra o relacionamento entre fornecedor e produto
         prd_ids = {
@@ -288,133 +424,11 @@ class WizardImportNfe(models.TransientModel):
         }
         self.env['product.supplierinfo'].create(prd_ids)
 
-    def checa_produtos(self):
-        if not self.nfe_xml:
-            raise UserError('Por favor, insira um arquivo de NFe.')
-        nfe_string = base64.b64decode(self.nfe_xml)
-        nfe = objectify.fromstring(nfe_string)
-
-        if not hasattr(nfe, 'NFe'):
-            raise UserError('Lamento, mas isso não é uma nota fiscal valida.')
-
-        company = self.get_partner(partner_find=nfe.NFe.infNFe.dest)
-        if not company:
-            raise UserError("Essa nota não é sua ou seu emitente não esta configurado corretamente.")
-
-        items = []
-        for det in nfe.NFe.infNFe.det:
-            item = self.carrega_produtos(det, nfe)
-            if item:
-                items.append(item.id)
-        self.wizard_produtos = self.env['wizard.produtos'].browse(items)
-        self.confirma = True
-
-        return {
-            'context': self.env.context,
-            'view_type': 'form',
-            'view_mode': 'form',
-            'res_model': 'wizard.import.nfe',
-            'res_id': self.id,
-            'view_id': False,
-            'type': 'ir.actions.act_window',
-            'target': 'new',
-        }
-
-    def carrega_produtos(self, item, nfe):
-        product_find = False
-        product_create = {'name': item.prod.xProd, 'uom_int': 1}
-
-        uom_id = self.env['product.uom'].search([
-            ('name', '=', str(item.prod.uCom))], limit=1).id
-
-        if uom_id:
-            product_create['uom_ext'] = uom_id
-
-        if item.prod.cEAN != 'SEM GTIN' and item.prod.cEAN != '':
-            product_find = self.env['product.product'].search([
-                ('barcode', '=', item.prod.cEAN)], limit=1)
-
-            if product_find:
-                product_create['product_id'] = product_find.id
-
-        if not product_find:
-            if hasattr(nfe.NFe.infNFe.emit, 'CNPJ'):
-                partner_doc = nfe.NFe.infNFe.emit.CNPJ.text
-            else:
-                partner_doc = nfe.NFe.infNFe.emit.CPF.text
-
-            partner_id = self.env['res.partner'].search([
-                ('cnpj_cpf', '=', self.arruma_cpf_cnpj(partner_doc))
-            ]).id
-            product_code = self.env['product.supplierinfo'].search([
-                ('product_code', '=', item.prod.cProd.text),
-                ('name', '=', partner_id)
-            ], limit=1)
-            product_find = self.env['product.product'].browse(product_code.product_tmpl_id.id)
-
-            if product_find:
-                product_create['product_id'] = product_find.id
-
-        return self.env['wizard.produtos'].create(product_create)
-
-    def get_partner(self, partner_find, create=False, custumer=False, supplier=False):
-        partner_doc = partner_find.CNPJ if hasattr(partner_find, 'CNPJ') else partner_find.CPF
-        partner_doc = str(partner_doc)
-        partner_doc = self.arruma_cpf_cnpj(partner_doc)
-        partner = self.env['res.partner'].search([
-            ('cnpj_cpf', '=', partner_doc)])
-
-        # Empresa não encontrado, devemos criar ?
-        if not partner and create:
-            # Ok, então vamos criar a empresa
-            city = self.env['res.state.city'].search([('name', '=ilike', partner_find.enderEmit.xMun)])
-            partner = {
-                'name': partner_find.xNome,
-                'is_company': True if hasattr(partner_find, 'CNPJ') else False,
-                'company_type': 'company' if hasattr(partner_find, 'CNPJ') else 'person',
-                'cnpj_cpf': partner_doc,
-                'supplier': True,
-            }
-
-            if custumer:
-                partner['custumer'] = True
-
-            if supplier:
-                partner['supplier'] = True
-
-            if hasattr(partner_find, 'IE') and hasattr(partner_find, 'CNPJ'):
-                partner['inscr_est'] = partner_find.IE.text
-                partner['indicador_ie_dest'] = '1'
-            elif not hasattr(partner_find, 'IE') and hasattr(partner_find, 'CNPJ'):
-                partner['indicador_ie_dest'] = '9'
-
-            if hasattr(partner_find, 'xFant'):
-                partner['legal_name'] = partner_find.xFant
-            else:
-                partner['legal_name'] = partner_find.xNome
-
-            if hasattr(partner_find, 'fone'):
-                partner['phone'] = partner_find.enderEmit.fone
-
-            if hasattr(partner_find, 'xCpl'):
-                partner['street2'] = partner_find.enderEmit.xCpl
-
-            partner['zip'] = partner_find.enderEmit.CEP
-            partner['default_supplier'] = 'supplier'
-            partner['street'] = partner_find.enderEmit.xLgr
-            partner['number'] = partner_find.enderEmit.nro
-            partner['district'] = partner_find.enderEmit.xBairro
-            partner['city_id'] = city.id
-            partner['state_id'] = city.state_id.id
-            partner['country_id'] = self.env.ref('base.br').id
-            partner = self.env['res.partner'].create(partner)
-
-        return partner
-
-    def retorna_data(self, date):
-        day = str(date).split('T')
-        hour = day[1].split('-')
-        datehour = day[0] + ' ' + hour[0]
-        datetime_obj = datetime.strptime(datehour, '%Y-%m-%d %H:%M:%S')
+    @staticmethod
+    def retorna_data(date):
+        data = str(date).split('T')
+        hora = data[1].split('-')
+        data_hora = data[0] + ' ' + hora[0]
+        datetime_obj = datetime.strptime(data_hora, '%Y-%m-%d %H:%M:%S')
 
         return datetime_obj
